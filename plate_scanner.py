4cb6472218509234023b2ad2a04a1b92067e5892
Matthew Miller <mnmiller1@me.com>
2017-07-15 16:15:22 +1000

    Switch to using OpenALPR

--- plate_scanner.py
+++ plate_scanner.py
@@ -1,44 +1,31 @@
-import sys
 import os
 
-sys.path.insert(1, os.path.join(os.path.dirname(os.path.abspath(__file__)),
-                                "BaseLib"))
-
-import DetectChars
-import cv2
-import DetectPlates
+from openalpr import Alpr
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 
-def run():
-    training_data = DetectChars.loadKNNDataAndTrainKNN()
 
-    if not training_data:
-        print "Invalid Training Data"
-        return
-
-    test_image = cv2.imread("BaseLib/LicPlateImages/6.png")
-    if test_image is None:
-        print "Failed to load test data"
-
-    plates = DetectPlates.detectPlatesInScene(test_image)
-    chars = DetectChars.detectCharsInPlates(plates)
+def run(file):
+    results = alpr.recognize_file(file)
 
     best_fit = None
 
-    for char in chars:
-        if best_fit is None:
-            best_fit = char.strChars
-        else:
-            if abs(len(char.strChars) - 6) < abs(len(best_fit) - 6):
-                best_fit = char.strChars
+    for plate in results['results']:
+        for candidate in plate['candidates']:
+            best_fit = candidate['plate']
+            if candidate['matches_template']:
+                break
 
-    print "Best Fit: " + best_fit
+    if best_fit is not None:
+        print "Found License: " + best_fit + " for file: " + file
+        #print get_page(best_fit)
+    else:
+        print "Failed to find a license plate for file: " + file
 
 
 def get_page(rego):
     driver = webdriver.Chrome()
-    driver.get("https://www.service.transport.qld.gov.au/checkrego/application/TermAndConditions.xhtml?windowId=9b2")
+    driver.get(
+        "https://www.service.transport.qld.gov.au/checkrego/application/TermAndConditions.xhtml?windowId=9b2")
     driver.find_element_by_id("tAndCForm:confirmButton").click()
     driver.find_element_by_id("vehicleSearchForm:plateNumber").send_keys(rego)
     driver.find_element_by_id("vehicleSearchForm:confirmButton").click()
@@ -46,7 +33,11 @@ def get_page(rego):
     driver.close()
     return src
 
+
 if __name__ == '__main__':
-    run()
-    page = get_page("")
-    print page
+    alpr = Alpr("au", "openalpr.conf", "")
+    alpr.set_top_n(20)
+    alpr.set_default_region("qld")
+
+    for filename in os.listdir("Car photos"):
+        run("Car photos/" + filename)
