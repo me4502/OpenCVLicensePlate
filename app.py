10554660cce5e0e9f00c404499ebcf9854819727
Matthew Miller <mnmiller1@me.com>
2017-07-16 10:18:23 +1000

    Added a function to upload photos

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
 import json
 import os
 
+from werkzeug.utils import secure_filename
+
 import plate_scanner
 
-from flask import Flask
+from flask import Flask, request, flash, redirect
 
 app = Flask(__name__)
 
 cache_file = "server_state.json"
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
+app.config['UPLOAD_FOLDER'] = 'Car photos'
 
 
 @app.route('/add_by_file/<filename>', methods=['GET'])
@@ -21,9 +25,32 @@ def add_by_file(filename):
     return json.dumps(result)
 
 
-@app.route('/scan_image', methods=['PUSH'])
+@app.route('/scan_image', methods=['GET', 'POST'])
 def post_image():
-    pass
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit a empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect('/add_by_file/' + filename)
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form method=post enctype=multipart/form-data>
+      <p><input type=file name=file>
+         <input type=submit value=Upload>
+    </form>
+    '''
 
 
 @app.route('/get_info/<int:number>', methods=['GET'])
@@ -32,6 +59,11 @@ def get_info(number=10):
     if os.path.exists(cache_file):
         images = json.loads(open(cache_file, "r").read())
     return json.dumps(images[-number:])
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
 if __name__ == '__main__':
